39453e906647ce16e12fc9904a1dbe123b198df9
wisdom rock <wisdomrock@gmail.com>
2025-12-07 12:35:32 -0500

    support OpenAiChatOptions

--- src/main/java/com/example/springai/config/ChatClientConfig.java
+++ src/main/java/com/example/springai/config/ChatClientConfig.java
@@ -1,8 +1,11 @@
 package com.example.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,4 +27,13 @@ public class ChatClientConfig {
         return chatClientBuilder.build();
     }
 
+    @Bean //injected using @Qualifier("openAiChatOptions")
+    ChatOptions openAiChatOptions(){
+        //SpringAI also support configure ChatOptions thru application yaml file
+        return OpenAiChatOptions.builder()
+                .model(OpenAiApi.ChatModel.CHATGPT_4_O_LATEST)
+                .maxTokens(500)
+                .maxCompletionTokens(200)
+                .build();
+    }
 }
