dc4d225ac3b5575737afcea4ffc911b79705c936
wisdom rock <wisdomrock@gmail.com>
2025-12-10 22:34:58 -0500

    have qdrant:v1.15 client and server working

--- src/main/java/com/example/springai/SpringAiApplication.java
+++ src/main/java/com/example/springai/SpringAiApplication.java
@@ -1,17 +1,26 @@
 package com.example.springai;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 /**
  * api-docs: http://localhost:8080/api-docs
  * swagger-ui: http://localhost:8080/swagger-ui/index.html
  */
+@Slf4j
 @SpringBootApplication
 public class SpringAiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAiApplication.class, args);
+        // Must Include system environment variables when start the app
+        // Get the value of the PATH environment variable
+        String pathValue = System.getenv("path");
+        Objects.requireNonNull(pathValue, "PATH environment variable not set on application");
+        log.info("OS PATH: {}", pathValue);
+    	SpringApplication.run(SpringAiApplication.class, args);
 	}
 
 }
